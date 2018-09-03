4438db68573462c78d035a5b8a9c44a5f7e162ed
winton <mail@wintoni.us>
2018-09-03 02:37:02 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# dot-store
+
+Immutable store extension for dot-event.
+
+![pale blue dot](https://qph.fs.quoracdn.net/main-qimg-347d2c178e6bf511ee5b91e8276c79fa)
+
+## Install
+
+```bash
+npm install --save dot-store
+```
+
+## Create store
+
+```js
+import createStore from "dot-store"
+const store = createStore()
+```
+
+## Use store
+
+```js
+await store.set("hello.world", true)
+store.get("hello.world") // true
+```
---
+++ test/indexTest.js
@@ -0,0 +1,54 @@
+import createStore from "../dist/core"
+
+test("get", () => {
+  const store = createStore({
+    state: { hello: { world: true } },
+  })
+  expect(store.get("hello.world")).toBe(true)
+})
+
+test("delete", async () => {
+  const store = createStore({
+    state: { hello: { world: true } },
+  })
+  await store.delete("hello.world")
+  expect(store.get("hello.world")).toBeUndefined()
+})
+
+test("merge", async () => {
+  const store = createStore({
+    state: { hello: { world: true } },
+  })
+
+  await store.merge("hello", { hi: true })
+  expect(store.get("hello")).toEqual({
+    hi: true,
+    world: true,
+  })
+})
+
+test("set", async () => {
+  const store = createStore()
+
+  await store.set("hello.world", true)
+  expect(store.get("hello.world")).toBe(true)
+
+  await store.set("hello.world", { hi: true })
+  expect(store.get("hello.world")).toEqual({ hi: true })
+})
+
+test("time", async () => {
+  const store = createStore()
+  await store.time("hello.world")
+  expect(store.get("hello.world")).toEqual(
+    expect.any(Number)
+  )
+})
+
+test("toggle", async () => {
+  const store = createStore({
+    state: { hello: { world: true } },
+  })
+  await store.toggle("hello.world")
+  expect(store.get("hello.world")).toBe(false)
+})
