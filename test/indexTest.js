faa03080f357003569fe3f344c8eadb9cd5d19ca
winton <mail@wintoni.us>
2018-09-12 23:38:47 -0400

    dot-event api changes

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,24 +1,24 @@
+import Events from "dot-event"
 import composeStore from "../dist/core"
 
 test("get", () => {
-  const store = composeStore({
-    state: { hello: { world: true } },
-  })
+  const state = { hello: { world: true } }
+  const store = composeStore(new Events({ state }))
+
   expect(store.get("hello.world")).toBe(true)
 })
 
 test("delete", async () => {
-  const store = composeStore({
-    state: { hello: { world: true } },
-  })
+  const state = { hello: { world: true } }
+  const store = composeStore(new Events({ state }))
+
   await store.delete("hello.world")
   expect(store.get("hello.world")).toBeUndefined()
 })
 
 test("merge", async () => {
-  const store = composeStore({
-    state: { hello: { world: true } },
-  })
+  const state = { hello: { world: true } }
+  const store = composeStore(new Events({ state }))
 
   await store.merge("hello", { hi: true })
   expect(store.get("hello")).toEqual({
@@ -28,7 +28,7 @@ test("merge", async () => {
 })
 
 test("set", async () => {
-  const store = composeStore()
+  const store = composeStore(new Events())
 
   await store.set("hello.world", true)
   expect(store.get("hello.world")).toBe(true)
@@ -38,7 +38,8 @@ test("set", async () => {
 })
 
 test("time", async () => {
-  const store = composeStore()
+  const store = composeStore(new Events())
+
   await store.time("hello.world")
   expect(store.get("hello.world")).toEqual(
     expect.any(Number)
@@ -46,9 +47,9 @@ test("time", async () => {
 })
 
 test("toggle", async () => {
-  const store = composeStore({
-    state: { hello: { world: true } },
-  })
+  const state = { hello: { world: true } }
+  const store = composeStore(new Events({ state }))
+
   await store.toggle("hello.world")
   expect(store.get("hello.world")).toBe(false)
 })
